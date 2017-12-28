17126f6274353e419e7f550f78fbd70c584f9d6b
essvee <s.vincent@nhm.ac.uk>
2017-12-28 12:32:41 +0000

    adds mappings

--- gbif_citations.py
+++ gbif_citations.py
@@ -1,4 +1,5 @@
 import requests
+import pymysql
 
 
 def citation_list():
@@ -9,6 +10,45 @@ def citation_list():
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print(e)
+
+    # dict to hold gbifDownloadKey: id pairs to return + pass to occurrence() later
+    download_keys = {}
+    separator = ', '
+
+    # Connect to database:
+    with pymysql.connect(host="localhost", user="root", password="r3ptar", db="dashboard") as cursor:
+
+        contents = r.json()['results']
+        for c in contents:
+            # Make sure record has an id, skip if not
+            if not c.get('id', None):
+                id = c['id']
+                abstract = c['abstract'] if 'abstract' in c else None
+                city = c['city'] if 'city' in c else None
+                content_type = c['contentType '] if 'contentType' in c else None
+                harvest_date = c['accessed'] if 'accessed' in c else None
+                doi = c['identifiers']['doi'] if 'doi' in c['identifiers'] else None
+                language = c['language'] if 'language' in c else None
+                literature_type = c['literatureType'] if 'literatureType' in c else None
+                open_access = c['openAccess'] if 'openAccess' in c else None
+                peer_review = c['peerReview'] if 'peerReview' in c else None
+                publisher = c['publisher'] if 'publisher' in c else None
+                source = c['source'] if 'source' in c else None
+                title = c['title'] if 'title' in c else None
+                year = c['year'] if 'year' in c else None
+                # Concatenate fields that may contain lists
+                countries_of_researcher = separator.join(c['countriesOfResearcher']) if 'countriesOfResearcher' \
+                                                                                        in c else None
+                topics = separator.join(c['topics']) if 'topics' in c else None
+                gbif_download_key = separator.join(c['gbifDownloadKey']) if 'gbifDownloadKey' in c else None
+                # Parse author name dicts
+                if 'authors' in c:
+                    namelist = []
+                    for n in c['authors']:
+                        namelist.append(' '.join(n.values()))
+                    authors = separator.join(namelist)
+            else:
+                continue
 
 
 def occurrence():
@@ -24,3 +64,7 @@ def dataset():
 def organizations():
     # Get organizational details
     return ""
+
+
+if __name__ == '__main__':
+    citation_list()
