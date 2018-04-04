0a38fad3d8092e5ff7f7f31b170bfb8be5a163f6
essvee <s.vincent@nhm.ac.uk>
2018-04-04 15:31:43 +0100

    Adds grouping by organization

--- gbif_occurrences.py
+++ gbif_occurrences.py
@@ -3,24 +3,50 @@ import pymysql
 
 
 def get_parts(citation_id):
-    # Get gbif keys - may be > 1 value
+    # Get gbif keys from local database - may be > 1 value
     g_keys = get_gbif_keys(citation_id)
 
     datasets = {}
     # For each gbif key, get the component dataset keys and record counts
     for k in g_keys:
-        dataset_list = get_dataset_keys(k)
-        for d in dataset_list:
-            # If datasets is duplicated, increment the record count accordingly
-            if d['d_key'] in datasets:
-                datasets[d['d_key']]['record_count'] += d['record_count']
+        for d in get_dataset_keys(k):
+            # Find out if we've seen this org before for this citation (might be different dataset)
+            d['orgKey'] = get_org_keys(d['d_key'])
+            d['orgName'] = get_org_names(d['orgKey'])
+
+            # If org is already there, increment the record count accordingly
+            if d['orgName'] in datasets:
+                datasets[d['orgName']]['record_count'] += d['record_count']
             else:
-                datasets[d['d_key']] = d
+                datasets[d['orgName']] = d
+
+    print(datasets.keys())
 
 
+# Use download keys for each dataset to organization key
+def get_org_keys(d_key):
+        try:
+            r = requests.get(f"http://api.gbif.org/v1/dataset/{d_key}")
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(e)
+
+        return r.json()['publishingOrganizationKey']
+
+
+def get_org_names(org_key):
+    try:
+        r = requests.get(f"http://api.gbif.org/v1/organization/{org_key}")
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+
+    return r.json()['title']
+
+
+# Get gbif download keys for one citation from local citations table
 def get_gbif_keys(citation_id):
-    # Get gbif download keys from citations table
-    with open('local_details.txt', 'r') as f:
+    with open('server-permissions.txt', 'r') as f:
         keys = f.read().splitlines()
         host, user, password, database = keys
 
@@ -39,6 +65,7 @@ def get_gbif_keys(citation_id):
     return g_keys
 
 
+# Use download keys to get details about other contributing datasets from GBIF
 def get_dataset_keys(g_key):
     try:
         r = requests.get(f"http://api.gbif.org/v1/occurrence/download/{g_key}/datasets?limit=100")
@@ -48,52 +75,12 @@ def get_dataset_keys(g_key):
 
     record_sets = []
 
+    # Get the info we're interested in for each other dataset and return
     for d in r.json()['results']:
-        dataset = {}
-        dataset['d_key'] = d['datasetKey']
-        dataset['record_count'] = d['numberRecords']
+        dataset = {'d_key': d['datasetKey'], 'record_count': d['numberRecords']}
         record_sets.append(dataset)
 
     return record_sets
-
-
-def count_datasets(temp_occurrences):
-    for d in temp_occurrences:
-        # For each gkey, insert into api url, parse results and get 'count'
-        try:
-            r = requests.get(f"http://api.gbif.org/v1/occurrence/download/{d}/datasets?limit=10")
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            print(e)
-
-        dataset_count = r.json()['count']
-        occurrences[d]['dataset_count'] = dataset_count
-
-    print(occurrences)
-
-
-def organizations():
-    try:
-        r = requests.get("http://api.gbif.org/v1/organization")
-        r.raise_for_status()
-        org_count = r.json()['count']
-
-        r = requests.get(f"http://api.gbif.org/v1/organization?limit={org_count}")
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(e)
-
-    results = r.json()['results']
-    datasets = 0
-    data_publishers = 0
-
-    for org in results:
-        if org['numPublishedDatasets'] > 0:
-            data_publishers += 1
-            datasets += org['numPublishedDatasets']
-
-    print(f"There are {datasets} datasets published to GBIF by {data_publishers} organizations.")
-
 
 
 occurrences = {}
