ac3e702cec0c29ea4bfeb28e3fc0ed0092c24046
essvee <s.vincent@nhm.ac.uk>
2018-04-13 13:38:58 +0100

    Retrieves deleted publishers as well as active

--- gbif_publishers.py
+++ gbif_publishers.py
@@ -8,23 +8,26 @@ def update_publishers():
     Looks at the GBIF Registry endpoint and adds any new publishers to local table
     """
     pub_keys = get_publishers()
-    try:
-        count = requests.get("http://api.gbif.org/v1/organization?limit=0").json()['count']
-        offset = 0
-        while count > offset:
-            results = requests.get(f"http://api.gbif.org/v1/organization?limit=200&offset={offset}").json()['results']
-            for p in results:
-                # If org is new, add the name, key and country to the table
-                if p['key'] not in pub_keys:
-                    publisher_name = p['title'].replace("\"", "\'").encode('ascii', 'ignore').decode('utf-8', 'ignore')
-                    publisher_country = p['country'] if 'country' in p else None
-                    db.query_db(f"""INSERT INTO gbif_publishers VALUES ("{p['key']}", "{publisher_name}",
-                    "{publisher_country}")""")
-            # Increment offset
-            offset += 200
+    urls = ["http://api.gbif.org/v1/organization/deleted", "http://api.gbif.org/v1/organization"]
 
-    except requests.exceptions.HTTPError as e:
-        print(e)
+    for url in urls:
+        try:
+            count = requests.get(f"{url}?limit=0").json()['count']
+            offset = 0
+            while count > offset:
+                results = requests.get(f"{url}?limit=200&offset={offset}").json()['results']
+                for p in results:
+                    # If org is new, add the name, key and country to the table
+                    if p['key'] not in pub_keys:
+                        publisher_name = p['title'].replace("\"", "\'").encode('ascii', 'ignore').decode('utf-8', 'ignore')
+                        publisher_country = p['country'] if 'country' in p else None
+                        db.query_db(f"""INSERT INTO gbif_publishers VALUES ("{p['key']}", "{publisher_name}",
+                        "{publisher_country}")""")
+                # Increment offset
+                offset += 200
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
 
 
 def get_publishers():
