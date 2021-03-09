df82950dc819f930fe01cb5f0919d0f083d9804f
essvee <s.vincent@nhm.ac.uk>
2021-03-09 13:54:33 +0000

    switch to using dbtools module

--- twitter.py
+++ twitter.py
@@ -1,47 +1,34 @@
 import tweepy
 import datetime
-import pymysql
+import gbif_dbtools as db
 
 
 def get_followers():
+    # get keys
+    consumer_key, consumer_secret, access_token, access_secret = db.get_keys('apikeys.txt')
+
     # OAuth process, using the keys and tokens
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
-    # Creation of the actual interface using authentication
     api = tweepy.API(auth)
+
+    # Get today's follower count for @NHM_Digitise
     follower_count = api.get_user('NHM_Digitise').followers_count
+
     # Get runtime + date
     today_dt = datetime.datetime.today().date()
 
-    # Connect to database
-    with pymysql.connect(host=host, user=user, password=password, db=database) as cursor:
-        # Write update to twitter_followers
-        # Get most recent follower count
-        cursor.execute("SELECT follower_count FROM twitter_followers "
-                       "WHERE DATE IN (SELECT MAX(date) FROM twitter_followers);")
-        result = cursor.fetchone()
-        # Calculate change in followers since last period
-        last_count = follower_count - result[0]
-        # Add new row and commit
-        sql = f"INSERT INTO twitter_followers(id, date, follower_count, change_followers) " \
-              f"VALUES(null, '{today_dt}', {follower_count} , {last_count});"
-        try:
-            cursor.execute(sql)
-        except pymysql.Error as e:
-            cursor.rollback()
+    # Connect to database and get last recorded follower count
+    sql_query = "SELECT follower_count FROM twitter_followers WHERE DATE IN (SELECT MAX(date) FROM twitter_followers);"
+    previous_follower_count = db.query_db(sql_query).fetchone()
 
+    # Calculate change in followers since last run
+    last_count = follower_count - previous_follower_count[0]
 
-# Get keys
-with open('apikeys.txt', 'r') as f:
-    keys = f.read().splitlines()
-
-consumer_key, consumer_secret, access_token, access_secret = keys
-
-# Get auth details + date
-with open('ckan-permissions.txt', 'r') as f:
-    s_keys = f.read().splitlines()
-
-host, user, password, database = s_keys
+    # Create update query, add new row and commit
+    sql_update = f"INSERT INTO twitter_followers (id, date, follower_count, change_followers) VALUES " \
+                 f"(null, '{today_dt}', {follower_count} , {last_count});"
+    db.query_db(sql_update)
 
 if __name__ == '__main__':
     get_followers()
