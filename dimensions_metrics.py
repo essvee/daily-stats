ddad27acb3609b9dab94bb24f319be68e17ba317
essvee <s.vincent@nhm.ac.uk>
2021-03-23 19:44:11 +0000

    formatting changes

--- dimensions_metrics.py
+++ dimensions_metrics.py
@@ -6,7 +6,7 @@ import time
 
 
 def get_citations():
-    doi_sql = "SELECT DISTINCT(doi) from gbif_citations WHERE DOI != 'None';"
+    doi_sql = "SELECT DISTINCT(doi) from gbif_citations WHERE doi != 'None';"
     insert_sql = "INSERT INTO gbif_bibliometrics (doi, times_cited, field_citation_ratio, relative_citation_ratio, harvest_date) " \
                 "VALUES (%s, %s, %s, %s, %s)"
 
@@ -20,16 +20,13 @@ def get_citations():
         # Throttle query rate to comply with API terms of use
         time.sleep(1)
         url = f"https://metrics-api.dimensions.ai/doi/{d[0]}"
+        print(url)
         try:
             r = requests.get(url)
             r.raise_for_status()
-            query_list.append((
-                f'{d[0]}',
-                r.json()['times_cited'] or 0,
-                r.json()['field_citation_ratio'] or 0,
-                r.json()['relative_citation_ratio'] or 0,
-                today_dt
-            ))
+            query_list.append((f'{d[0]}', r.json()['times_cited'] or 0,
+                               r.json()['field_citation_ratio'] or 0, r.json()['relative_citation_ratio'] or 0,
+                               today_dt))
         # Skip over DOIs which aren't found
         except HTTPError:
             continue
