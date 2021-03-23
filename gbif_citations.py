49e1c350a7ba6dc02a58f3d64b0a9573c2cb9301
essvee <s.vincent@nhm.ac.uk>
2021-03-23 19:50:27 +0000

    refactored to work with literature endpoint

--- gbif_citations.py
+++ gbif_citations.py
@@ -5,33 +5,24 @@ import gbif_dbtools as db
 import gbif_occurrences as go
 import itertools
 
-new_ids = []
-
 
 def get_list():
     """
     Get list of NHM data citations from GBIF api
     """
-
     works = []
 
     # Get list of works that cited NHM specimens
     try:
         for offset in itertools.count(step=100):
-            # url =f"https://www.gbif.org/api/resource/search?contentType=literature&limit=100&offset={offset}" \
-            #      f"&gbifDatasetKey=7e380070-f762-11e1-a439-00145eb45e9a"
-            url =f"https://api.gbif.org/v1/literature/search?gbifDataSetKey=7e380070-f762-11e1-a439-00145eb45e9a" \
-                 f"&limit=100&offset={offset}"
+            url = f"https://api.gbif.org/v1/literature/search?gbifDataSetKey=7e380070-f762-11e1-a439-00145eb45e9a" \
+                  f"&limit=100&offset={offset}"
             r = requests.get(url)
             r.raise_for_status()
             results = r.json()['results']
 
-            # Cycle through contents and skip any without an id
-            for result in results:
-                if 'id' not in result:
-                    continue
-                else:
-                    works.append(result)
+            # Only keep results with an id
+            works.extend([result for result in results if 'id' in result])
 
             if r.json()['endOfRecords']:
                 break
@@ -39,7 +30,6 @@ def get_list():
     except requests.exceptions.HTTPError as e:
         print(e)
 
-    print(f"no. of results being passed to map_fields(): {len(works)}")
     map_fields(works)
 
 
@@ -52,24 +42,12 @@ def get_doi(record):
 
 
 def parse_author_names(record):
-
     if 'authors' in record:
         namelist = []
         for n in record['authors']:
-            namelist.append(' '.join(n.values()))
-        return '; '.join(namelist).replace("'", "''")
-    else:
-        return None
-
-
-def get_gbif_download_key(record):
-    # Todo: amend this function so it sparks the occurrence-aggregation/contributor data process
-    # List of one to many dataset IDs that were used in the paper
-    if 'gbifDownloadKey' in record:
-        download_keys = '; '.join(record['gbifDownloadKey'])
-        if len(download_keys) > 12000:
-            print(f"download key length for {record['id']}: {len(download_keys)} ")
-        return download_keys
+            full_name = ' '.join(n.values())
+            namelist.append(full_name)
+        return '; '.join(namelist)
     else:
         return None
 
@@ -77,7 +55,7 @@ def get_gbif_download_key(record):
 def map_fields(works):
     """
     Flatten json and add default values where required
-    :param contents: json object containing info about NHM data citations
+    :param works: list of dicts each containing info about a single NHM data citation
     """
     separator = '; '
     all_citations = {}
@@ -85,23 +63,27 @@ def map_fields(works):
     for c in works:
         citation_dict = {'update_date': datetime.datetime.strptime(c['modified'][0:10], "%Y-%m-%d").date(),
                          'gid': c['id'],
-                         'abstract': c['abstract'].replace("'", "''") if 'abstract' in c else None,
+                         'abstract': c['abstract'].replace("\n", "") if 'abstract' in c else None,
                          'harvest_date': c['discovered'] if 'discovered' in c else '1111-01-01',
                          'pub_date': c['published'][0:10] if 'published' in c else '1111-01-01',
                          'language': c['language'] if 'language' in c else None,
                          'literature_type': c['literatureType'] if 'literatureType' in c else None,
                          'open_access': c['openAccess'] if 'openAccess' in c else None,
                          'peer_review': c['peerReview'] if 'peerReview' in c else None,
-                         'publisher': c['publisher'].replace("'", "''") if 'publisher' in c else None,
-                         'source': c['source'].replace("'", "''") if 'source' in c else None,
-                         'title': c['title'].replace("'", "''") if 'title' in c else None,
+                         'publisher': c['publisher'].replace("\n", "") if 'publisher' in c else None,
+                         'source': c['source'].replace("\n", "") if 'source' in c else None,
+                         'title': c['title'].replace("\n", "") if 'title' in c else None,
                          'year': c['year'] if 'year' in c else None, 'month': c['month'] if 'month' in c else 0,
                          'countries_of_researcher': separator.join(c['countriesOfResearcher'])
                          if 'countriesOfResearcher' in c else None,
-                         'topics': separator.join(c['topics']) if 'topics' in c else None,
+                         'topics': separator.join(c['topics']) if ('topics' in c and len(c['topics']) > 0) else None,
                          'doi': get_doi(c),
                          'authors': parse_author_names(c),
-                         'gbif_download_key': get_gbif_download_key(c)}
+                         'gbif_download_count': len(c['gbifDownloadKey']) if 'gbifDownloadKey' in c else None,
+                         'nhm_record_count': None,
+                         'total_record_count': None,
+                         'total_dataset_count': None,
+                         'gbif_dk_list': c['gbifDownloadKey'] if 'gbifDownloadKey' in c else None}
 
         # Adds this citation to the overall list
         all_citations[c['id']] = citation_dict
@@ -113,20 +95,16 @@ def triage_citations(works):
     """
     Identify citations to be removed, added and updated on database
     """
-    # Set of current citation haul IDs, to check against
+    # Keys from current API call
     api_citation_ids = set(works.keys())
-
-    # Ditto for ids currently in the database.
+    # Keys and records on database
     existing_citations = db.query_db("SELECT id, update_date FROM gbif_citations;").fetchall()
     existing_citation_ids = set([c[0] for c in existing_citations])
-
-    # Set of IDs which are in the database, but NOT in the api results any more. Need removing.
+    # Anything in the db but not the API needs deleting from the db
     citation_ids_to_delete = list(existing_citation_ids - api_citation_ids)
-
-    # Set of IDs which are not in the database, but are in the api results. Need adding.
+    # Anything in the api and not the db needs adding to the db
     citation_ids_to_add = list(api_citation_ids - existing_citation_ids)
-
-    # Set of IDs that are in both payload and database. Some might need updating.
+    # Anything in both needs to be checked for latest update
     common_citation_ids = existing_citation_ids & api_citation_ids
 
     # Identify updated records since last run and queue them for deletion and re-insertion
@@ -135,14 +113,10 @@ def triage_citations(works):
             # get matching result from api payload
             api_record = works[x[0]]
 
-            # compare modified and update_date
-            # if modified is > updated, api result is newer
+            # compare modified and update_date. If api result update > database update date, delete and reload
             if api_record['update_date'] > x[1]:
                 citation_ids_to_delete.append(x[0])
                 citation_ids_to_add.append(x[0])
-            # if not, ignore
-            else:
-                continue
 
     # Remove redacted records and records in need of updating
     if len(citation_ids_to_delete) > 0:
@@ -151,10 +125,7 @@ def triage_citations(works):
     # Get brand new + updated/to be re-added citation records from api payload
     if len(citation_ids_to_add) > 0:
         # Generate list containing new row values
-        citations_to_add = []
-        for citation_id in citation_ids_to_add:
-            citations_to_add.append(works[citation_id])
-
+        citations_to_add = [works[p] for p in citation_ids_to_add]
         add_citations(citations_to_add)
 
 
@@ -162,7 +133,7 @@ def remove_citations(citation_ids):
     # Delete statement: generate and execute in a batch
     sql = "DELETE FROM gbif_citations where id = %s"
 
-    # List of ids that to be deleted from gbif_citations and cascades across related tables
+    # List of ids to be deleted from gbif_citations (cascades across related tables)
     params = citation_ids
 
     # Trigger delete operation. Cascades to related records in gbif_occurrences and gbif_bibliometrics
@@ -170,41 +141,36 @@ def remove_citations(citation_ids):
 
 
 def add_citations(new_citations):
-    try:
-        # adds/updates new/amended records
-        for c in new_citations:
-            insert_sql = f"INSERT INTO gbif_citations(abstract, authors, " \
-                  f"countries_of_researcher, gbif_download_key," \
-                  f"id, harvest_date, doi, language, literature_type, open_access," \
-                  f"peer_review, publisher, source, title, topics, update_date, year, month, pub_date)" \
-                  f"VALUES('{c['abstract']}','{c['authors']}'," \
-                  f"'{c['countries_of_researcher']}', '{c['gbif_download_key']}', '{c['gid']}'," \
-                  f"'{c['harvest_date']}', '{c['doi']}', '{c['language']}', '{c['literature_type']}'," \
-                  f"'{c['open_access']}', '{c['peer_review']}', '{c['publisher']}', '{c['source']}'," \
-                  f"'{c['title']}', '{c['topics']}', '{c['update_date'].strftime('%Y-%m-%d')}', {c['year']}, {c['month']}," \
-                  f"'{c['pub_date']}');"
+    # structures to hold row_data for insertion
+    row_data = []
+    insert_query = "INSERT INTO gbif_citations(abstract, authors, countries_of_researcher, id, harvest_date, " \
+                   "doi, language, literature_type, open_access, peer_review, publisher, source, title, " \
+                   "topics, update_date, year, month, pub_date, total_dataset_count, total_record_count, " \
+                   "nhm_record_count) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, " \
+                   "%s, %s, %s, %s, %s)"
 
-            sql = insert_sql.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-            db.query_db(sql)
+    # Call occurrence script here: should take new_citations and return a list of the same record with total
+    # publisher count, total record count, nhm record count fields added and populated
+    for record in new_citations:
+        if record['gbif_dk_list']:
+            record = go.assemble_parts(record)
+
+        # add/update new/amended records
+        row_data.append((record['abstract'], record['authors'], record['countries_of_researcher'], record['gid'],
+                         record['harvest_date'], record['doi'], record['language'], record['literature_type'],
+                         record['open_access'], record['peer_review'], record['publisher'], record['source'],
+                         record['title'], record['topics'], record['update_date'].strftime('%Y-%m-%d'), record['year'],
+                         record['month'], record['pub_date'], record['total_dataset_count'],
+                         record['total_record_count'], record['nhm_record_count']))
+
+        print(record['title'])
+
+    try:
+        db.update_db(insert_query, row_data)
 
     except pymysql.Error as e:
-        print(sql)
-        print(e)
-
-
-def occurrences():
-    """
-    Triggers gbif_occurrences script to run for any new citation ids
-    """
-    try:
-        for n in new_ids:
-            go.assemble_parts(n)
-
-    except pymysql.Error as e:
-        print(n)
-        print(e)
+        print(e.args)
 
 
 if __name__ == '__main__':
     get_list()
-#    occurrences()
